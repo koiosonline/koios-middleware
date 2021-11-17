c583519ef4568803333687533a2a0869ef897abe
Stephan Kop <stephan.kop@outlook.com>
2021-11-17 18:11:30 +0100

    added variable port for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import schedule from 'node-schedule';
 import handleCalendar from "./handlers/calendarHandler.js";
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 dotenv.config();
 
 // Callback to get data on the first load
