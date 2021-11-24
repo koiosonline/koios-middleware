82f29a8c41861f580ecb542af85d154e2d791234
Stephan Kop <stephankop@stephans-mbp.home>
2021-11-24 11:41:18 +0100

    added cors

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ const calendarSchedule = schedule.scheduleJob('* 1 * * *', async () => {
   await fetchCalendar()
 })
 
+app.use((req, res, next) => {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://koios.world');
+  res.setHeader('Access-Control-Allow-Origin', 'https://app.koios.world');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Koios middleware')
 });
