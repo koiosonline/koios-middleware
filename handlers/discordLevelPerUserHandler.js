02138074cf4583ac11040dbdb185a8280656df77
Stephan Kop <stephankop@stephans-mbp.home>
2021-12-08 12:05:18 +0100

    added console log

--- handlers/discordLevelPerUserHandler.js
+++ handlers/discordLevelPerUserHandler.js
@@ -4,6 +4,7 @@ const handleDiscordLevelPerUser = async (user) => {
   const callData = store.get('discordLevels');
   if (callData) {
     const filtered = callData.players.filter(player => player.username.toLowerCase() === user.toLowerCase())
+    console.log(filtered);
     return await filtered;
   }
 }
