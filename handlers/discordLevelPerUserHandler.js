96fe658c81eac99b5ce5793ebf15e92259cc6182
Stephan Kop <stephankop@stephans-mbp.home>
2021-12-08 14:57:52 +0100

    added all pages together

--- handlers/discordLevelPerUserHandler.js
+++ handlers/discordLevelPerUserHandler.js
@@ -3,9 +3,14 @@ import store from 'store'
 const handleDiscordLevelPerUser = async (user) => {
   const callData = store.get('discordLevels');
   if (callData) {
-    const filtered = callData.players.filter(player => player.username.toLowerCase() === user.toLowerCase())
-    console.log(filtered);
-    return await filtered;
+    let foundUsername;
+    const filtered = callData.map((element) => {
+      const result = element.filter(callData => callData.username.toLowerCase() === user.toLowerCase())
+      if (result.length >= 1) {
+        foundUsername = result
+      }
+    })
+    return await foundUsername;
   }
 }
 
--- services/fetchDiscordLevels.js
+++ services/fetchDiscordLevels.js
@@ -4,13 +4,21 @@ import store from 'store'
 const fetchDiscordLevels = async () => {
   const guildId = process.env.GUILD_ID
   const url = `https://mee6.xyz/api/plugins/levels/leaderboard/${guildId}`
+  const pages = 7
+  let totalData = []
 
-  await axios.get(url)
+  for (let i = 0; i <= pages; i++) {
+    const url = `https://mee6.xyz/api/plugins/levels/leaderboard/${guildId}?page=${i}`
+    await axios.get(url)
     .then((res) => {
-      const data = res.data
-      store.set('discordLevels', data);
+      const data = res.data.players
+      totalData.push(data)
+      if (i === 7) {
+        store.set('discordLevels', totalData);
+      }
       return
     })
+  }
 }
 
 export default fetchDiscordLevels
