5d8c19bdb973b96324a703833a25b932f3653aad
Stephan Kop <stephankop@stephans-mbp.home>
2021-12-15 11:38:17 +0100

    small async change

--- handlers/discordLevelPerUserHandler.js
+++ handlers/discordLevelPerUserHandler.js
@@ -4,13 +4,13 @@ const handleDiscordLevelPerUser = async (user) => {
   const callData = store.get('discordLevels');
   if (callData) {
     let foundUsername;
-    const filtered = callData.map((element) => {
+    callData.map((element) => {
       const result = element.filter(callData => callData.username.toLowerCase() === user.toLowerCase())
       if (result.length >= 1) {
         foundUsername = result
       }
     })
-    return await foundUsername;
+    return foundUsername;
   }
 }
 
